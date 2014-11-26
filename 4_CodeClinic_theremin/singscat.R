f2604a8fb2fe98a1322c08cf37d6149839ae9a26
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-11-26 03:59:05 -0800

    testing macintosh system speech

---
+++ 4_CodeClinic_theremin/singscat.R
@@ -0,0 +1,12 @@
+# macintosh speech documentation at https://developer.apple.com/library/mac/documentation/userexperience/conceptual/SpeechSynthesisProgrammingGuide/FineTuning/FineTuning.html#//apple_ref/doc/uid/TP40004365-CH5-SW4
+
+SingScat <- function() {
+  #for (thePitch in 34:80) {
+  #  print(thePitch)
+  #  system(paste("say [[pbas ", thePitch, "]]bah"))
+  #}
+  for(theVolume in 0:9) {
+    print(theVolume)
+    system(paste("say [[volm .", theVolume, "]]bah",sep=""))
+  }
+}
