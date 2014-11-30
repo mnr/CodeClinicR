ae7508fb74279f02b66b227b14a9548a3f0944d8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-11-29 22:30:28 -0800

    this works as specified.

--- 4_CodeClinic_theremin/theremin.R
+++ 4_CodeClinic_theremin/theremin.R
@@ -1,12 +1,18 @@
 require(tcltk)
 
+kPitchRange <- 80:34 #pitch can be from 34 to 80. 
+kPitchRangeLength <- length(kPitchRange)
+
 handleMouse <- function(x,y) {
-  #theVolume in 0:9
-  #thePitch in 34:80
+  print(c(x,y))
+  theVolume <- as.numeric(x)/100 #volume can be from .0 to .9
+  # y can be from 0 to 200. 0 is at top so I've inverted the list
+  thePitchIndex <- kPitchRangeLength* as.numeric(y) / 200
+  thePitch <- kPitchRange[thePitchIndex]
   
-  volm.string <- paste("[[volm .", theVolume, "]]",sep="")
+  volm.string <- paste("[[volm ", theVolume, "]]",sep="")
   pbas.string <- paste("[[pbas ", thePitch, "]]",sep="")
-  all.string <- paste("say ",volm.string,pbas.string,"bah")
+  all.string <- paste("say ",volm.string, pbas.string, "bah")
   print(all.string)
   system(all.string)
 }
