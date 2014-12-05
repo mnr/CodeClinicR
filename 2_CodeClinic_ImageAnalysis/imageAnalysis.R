2612f2b60e18a01949a788a4a0f7d96ede9fcef3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-05 13:29:35 -0800

    exploring image packages

---
+++ 2_CodeClinic_ImageAnalysis/imageAnalysis.R
@@ -0,0 +1,10 @@
+# http://www.r-bloggers.com/r-image-analysis-using-ebimage/
+# http://www.bioconductor.org/packages/release/bioc/html/EBImage.html
+# Rimage offers diverse filtering functions but supports only the 
+#   JPEG format and cannot save images. 
+# The package ripa is dedicated to the analysis of hyperspectral 
+#   images but does not provide for image segmentation. 
+# biOps offers a wide range of image filters, but only supports 
+#   the JPEG and TIFF formats and lacks a fast interactive display interface. 
+# The recent package RImageJ provides R bindings to ImageJ, 
+#   but does not allow easy access to the image data by R. 
