a01fe0124073f57398499ec2d94706ce0d853f5e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-07 10:53:47 -0800

    trying to find tools to match images.

--- 2_CodeClinic_ImageAnalysis/imageAnalysis.R
+++ 2_CodeClinic_ImageAnalysis/imageAnalysis.R
@@ -1,10 +1,22 @@
-# http://www.r-bloggers.com/r-image-analysis-using-ebimage/
-# http://www.bioconductor.org/packages/release/bioc/html/EBImage.html
-# Rimage offers diverse filtering functions but supports only the 
-#   JPEG format and cannot save images. 
-# The package ripa is dedicated to the analysis of hyperspectral 
-#   images but does not provide for image segmentation. 
-# biOps offers a wide range of image filters, but only supports 
-#   the JPEG and TIFF formats and lacks a fast interactive display interface. 
-# The recent package RImageJ provides R bindings to ImageJ, 
-#   but does not allow easy access to the image data by R. 
+# http://cran.r-project.org/web/packages/ripa/index.html
+install.packages("ripa")
+library(ripa)
+
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("EBImage")
+browseVignettes("EBImage")
+library("EBImage")
+f = system.file("images", "sample.png", package="EBImage")
+img = readImage(f)
+
+install("jpeg")
+img <- readJPEG(system.file("img", "Rlogo.jpg", package="jpeg"))
+#match()
+
+if (exists("rasterImage")) { # can plot only in R 2.11.0 and higher
+  plot(2:2, type='n')
+  rasterImage(img, 1.2, 1.27, 1.8, 1.73)
+  #rasterImage(img.n, 1.5, 1.5, 1.9, 1.8)
+}
+
