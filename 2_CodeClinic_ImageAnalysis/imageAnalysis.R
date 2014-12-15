80fc51b39a529f1924ba56d7feb3931c8619f010
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-15 12:10:13 -0800

    trying to load EBImage

--- 2_CodeClinic_ImageAnalysis/imageAnalysis.R
+++ 2_CodeClinic_ImageAnalysis/imageAnalysis.R
@@ -2,20 +2,21 @@
 install.packages("ripa")
 library(ripa)
 
-<<<<<<< HEAD
+# # # # # #
 # EBImage
 =======
-# try grepraw
-
-
->>>>>>> c2670a1cf46d4e3816d27d16dab20663a8fa8f81
 source("http://bioconductor.org/biocLite.R")
 biocLite("EBImage")
-browseVignettes("EBImage")
+install.packages("tiff") #package ‘tiff’ is not available (for R version 3.1.2)
 library("EBImage")
+# Error in loadNamespace(j <- i[[1L]], c(lib.loc, .libPaths()), versionCheck = vI[[j]]) : 
+# there is no package called ‘tiff’
+# Error: package or namespace load failed for ‘EBImage’
+browseVignettes("EBImage")
 f = system.file("images", "sample.png", package="EBImage")
 img = readImage(f)
 
+# # # # # #
 # grep, grepRaw, grepl
 install.packages("jpeg")
 library(jpeg)
@@ -28,9 +29,11 @@ grepRaw(as.vector(img.one.subset[,,1]), as.vector(img.one[,,1]), value=FALSE)
 # In grepl(img.one.subset[, , 1], img.one[, , 1], useBytes = TRUE) :
 #  argument 'pattern' has length > 1 and only the first element will be used
 
+# # # # # #
 # package Raster
 #http://cran.r-project.org/web/packages/raster/index.html
 
+# # # # # #
 # package rimage
 install.packages("rimage")
 # Warning in install.packages : package ‘rimage’ is not available (for R version 3.1.2)
