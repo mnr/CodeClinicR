f0be60ce5b7a8c7c3787825ec3aa2c1a524cf25d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-17 02:17:47 -0800

    set up to iterate through list of files

--- 2_CodeClinic_ImageAnalysis/test.R
+++ 2_CodeClinic_ImageAnalysis/test.R
@@ -1,2 +1,13 @@
 # test the function
 
+setwd("2_CodeClinic_ImageAnalysis")
+source("isthisacroppedversionofthat.R")
+
+list.of.images <- list.files("imagesToAnalyze")
+image.pairs <- expand.grid(list.of.images,list.of.images)
+funcWrapper <- function(anImagePair) {
+  needle <- paste("imagesToAnalyze",anImagePair[1])
+  haystack <- paste("imagesToAnalyze",anImagePair[2])
+  isthisacroppedversionofthat(needle,haystack)
+}
+apply(image.pairs,1,funcWrapper)
