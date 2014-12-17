688f707b2d88149b55a4846ee3b5b22481b2a9f3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-17 01:24:03 -0800

    opening up file to create function

---
+++ 2_CodeClinic_ImageAnalysis/isthisacroppedversionofthat.R
@@ -0,0 +1,8 @@
+# function to test if one image is a cropped version of another
+# needle - image that might be a cropped version
+# haystack - image that might be a parent version
+# returns TRUE or FALSE
+
+isthisacroppedversionofthat <- function(needle,haystack) {
+  
+}
