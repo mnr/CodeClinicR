83babb775ab7b00b248878788723d280dda375a8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2014-12-17 02:47:31 -0800

    remove library raster. raster() doesn't recognize jpeg. rgdal won't compile with maverick

--- 2_CodeClinic_ImageAnalysis/test.R
+++ 2_CodeClinic_ImageAnalysis/test.R
@@ -1,13 +1,17 @@
-# test the function
+# set up for test
+
+install.packages("jpeg")
+library(jpeg)
 
 setwd("2_CodeClinic_ImageAnalysis")
 source("isthisacroppedversionofthat.R")
 
+# test the function
 list.of.images <- list.files("imagesToAnalyze")
 image.pairs <- expand.grid(list.of.images,list.of.images)
 funcWrapper <- function(anImagePair) {
-  needle <- paste("imagesToAnalyze",anImagePair[1])
-  haystack <- paste("imagesToAnalyze",anImagePair[2])
+  needle <- paste("imagesToAnalyze/",anImagePair[1],sep="")
+  haystack <- paste("imagesToAnalyze/",anImagePair[2],sep="")
   isthisacroppedversionofthat(needle,haystack)
 }
 apply(image.pairs,1,funcWrapper)
