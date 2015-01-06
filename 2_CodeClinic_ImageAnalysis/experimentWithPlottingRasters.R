cb5df8d4ce615fca2145c7f8e1f8f1d01c8f4b81
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-01-06 09:20:27 -0800

    ongoing experiments

--- 2_CodeClinic_ImageAnalysis/experimentWithPlottingRasters.R
+++ 2_CodeClinic_ImageAnalysis/experimentWithPlottingRasters.R
@@ -1,15 +1,19 @@
 # testing plotting functions
 
-plotThisJpeg <- myjpeg
+plotThisJpeg <- haystack.raster
 ptj.width <- ncol(plotThisJpeg)
 ptj.height <- nrow(plotThisJpeg)
-plotThisClip <- myjpegclip
+plotThisClip <- needle.raster
 ptjc.width <- ncol(plotThisClip)
 ptjc.height <- nrow(plotThisClip)
 
-plot(c(100, ptj.width), c(100, ptj.height), type = "n")
+plot(c(0, ptj.width), c(0, ptj.height), type = "n")
 # plot(plotThisJpeg,useRaster=TRUE)
 # plot(plotThisClip,add=TRUE,useRaster=TRUE)
-rasterImage(plotThisJpeg, 100, 100, ptj.width, ptj.height)
+rasterImage(plotThisJpeg, 0, 0, ptj.width, ptj.height)
 
 rasterImage(plotThisClip, 500, 500, ptjc.width, ptjc.height)
+this <- arrayInd(points.of.interest[1000],dim(haystack.raster))
+xleft <- this[2]
+ybottom <- this[1]
+rect(xleft,ybottom,(ptjc.width+xleft),(ptjc.height+ybottom))
