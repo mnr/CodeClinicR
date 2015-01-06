662a82458d6acca806028f895034332a8caf49e7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-01-06 09:21:51 -0800

    It isn't reasonable to identify the first pixel of needle as the first pixel of a subset. jpeg is lossy, so any recognition needs to be a correlation, not an equality. I've removed that code, refigured the list of interesting points, and will now add a "brick" correlation.

--- 2_CodeClinic_ImageAnalysis/isthisacroppedversionofthat.R
+++ 2_CodeClinic_ImageAnalysis/isthisacroppedversionofthat.R
@@ -15,31 +15,12 @@ isthisacroppedversionofthat <- function(needle,haystack) {
   if ((needle.height >= nrow(haystack.raster)) 
       && (needle.width >= ncol(haystack.raster))) return(c(0,0)) 
   
-  # find the first byte of needle in haystack
-  points.of.interest <- which(haystack.raster == needle.raster[1])
-  # if there are no instances of the first byte of needle in haystack, 
-  # then this can't be a cropped version
-  if (length(points.of.interest) == 0) return(c(0,0))
+  points.of.interest <- ncol(haystack.raster) * nrow(haystack.raster) # number of pixels in one layer
+  wide.enough <- which(arrayInd(points.of.interest,dim(haystack.raster))[,2] < needle.width)
+  tall.enough <- which(arrayInd(wide.enough,dim(haystack.raster))[,1] < needle.height)
+
   
-  # quick and dirty matching to filter down possible matches
-  for (loopIndex in 1:2) {
-    next.point.of.interest <- which(haystack.raster[points.of.interest + loopIndex] == needle.raster[loopIndex + 1])
-    points.of.interest <- points.of.interest[next.point.of.interest]
-    if (length(points.of.interest) == 0) return(c(0,0))
-  }
-  
-  # find dim (aka:row-column-layer) of index points in points.of.interest
-  location.of.POIs <- arrayInd(points.of.interest,dim(haystack.raster))
-  
-  # remove points.of.interest that are not wide enough to hold the sub image
-  points.of.interest <- points.of.interest[location.of.POIs[,2] < needle.width]
-  
-  # remove points.of.interest that are not tall enough to hold the sub image
-  points.of.interest <- points.of.interest[location.of.POIs[,1] < needle.height] 
-  points.of.interest <- na.omit(points.of.interest) #I may be subsetting incorrectly, so use this to remove NA
-  
-  if (length(points.of.interest) == 0) return(c(0,0))
-  
+    
   # now points.of.interest are interesting. But are they subsets?
   # Compare the first row of needle against the rows associated with points.of.interest
   # first - subset the first row and Red layer of needle to speed things up
