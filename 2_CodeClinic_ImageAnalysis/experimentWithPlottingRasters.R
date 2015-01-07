879a4517f162bbaf6bd58b595fe5e2d26a7e5d1c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-01-07 09:33:39 -0800

    using this area for experiments with code

--- 2_CodeClinic_ImageAnalysis/experimentWithPlottingRasters.R
+++ 2_CodeClinic_ImageAnalysis/experimentWithPlottingRasters.R
@@ -1,9 +1,9 @@
 # testing plotting functions
 
-plotThisJpeg <- haystack.raster
+plotThisJpeg <- myjpeg
 ptj.width <- ncol(plotThisJpeg)
 ptj.height <- nrow(plotThisJpeg)
-plotThisClip <- needle.raster
+plotThisClip <- myjpegclip
 ptjc.width <- ncol(plotThisClip)
 ptjc.height <- nrow(plotThisClip)
 
@@ -12,8 +12,19 @@ plot(c(0, ptj.width), c(0, ptj.height), type = "n")
 # plot(plotThisClip,add=TRUE,useRaster=TRUE)
 rasterImage(plotThisJpeg, 0, 0, ptj.width, ptj.height)
 
+rasterImage(plotThisJpeg[1:1500,1:2500,1], 0, 0, ptj.width, ptj.height)
+
 rasterImage(plotThisClip, 500, 500, ptjc.width, ptjc.height)
+
 this <- arrayInd(points.of.interest[1000],dim(haystack.raster))
 xleft <- this[2]
 ybottom <- this[1]
 rect(xleft,ybottom,(ptjc.width+xleft),(ptjc.height+ybottom))
+
+corltHayToNeedle <- function (aPOI) {
+  needle.height.offset <- needle.quarter.height + aPOI
+  needle.width.offset <- needle.quarter.width + aPOI
+  ccf(haystack.raster[aPOI:needle.height.offset,1:needle.width.offset,1],needle.brick)
+}
+
+applyResults <- lapply(points.of.interest,corltHayToNeedle)
