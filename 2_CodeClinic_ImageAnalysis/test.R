1137bf9d205573fa98b25cca2a8a56dbb0458fc0
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-02-01 20:47:42 -0800

    added a case that doesn't work

--- 2_CodeClinic_ImageAnalysis/test.R
+++ 2_CodeClinic_ImageAnalysis/test.R
@@ -22,8 +22,10 @@ list.of.images <- list.files("imagesToAnalyze")
 image.pairs <- expand.grid(list.of.images,list.of.images)
 apply(image.pairs,1,funcWrapper)
 
+
 # test known subsets
 funcWrapper(c("460249177a.jpg","460249177.jpg")) #this is a subset
 funcWrapper(c("478946583a.jpg","478946583.jpg")) #this is a subset
 funcWrapper(c("78771293a.jpg","78771293.jpg")) #this is a subset
 funcWrapper(c("103992931.jpg","78771293.jpg")) #this is not a subset
+funcWrapper(c("168680522.jpg","103992931.jpg")) #this is not a subset
