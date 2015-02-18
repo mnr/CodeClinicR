6d3f09880805d2d6f7e592f033e04524b5505b88
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-02-18 14:44:01 -0800

    factored object to allow for getPORDataRange alongside loadAYearOfData

--- 1_CodeClinic_PendOReille/LakePendOreilleData.R
+++ 1_CodeClinic_PendOReille/LakePendOreilleData.R
@@ -1,10 +1,11 @@
 # Lake Pend Oreille S3 class to abstract the website data from the program
 # Author: Mark Niemann-Ross, Spring 2015
+# uses the local environment approach
+# http://cran.r-project.org/doc/manuals/r-release/R-lang.html#Argument-evaluation
 # recommended reading: http://www.cyclismo.org/tutorial/R/s3Classes.html
 # assumes presence of lubridate for date conversion
 
 LakePendOreilleData <- function() {
-  
   kPORDeepMoorPath <- "http://lpo.dt.navy.mil/data/"
   
   # make sure the site is live
@@ -27,31 +28,58 @@ LakePendOreilleData <- function() {
                             Wind_Speed = numeric()
   )
   YearsOfData$datetime <- as.POSIXct(YearsOfData$datetime)
+  this.yearsOfDataAlreadyLoaded <- c("")
   
   # get a list of available years of data
   # stubbing this out for now until navy.mil stabilizes their naming conventions
   availableYearsOfData <- c("2011","2012","2013","2014")
   
-  # create a list of functions for this object
-  me <- list (
+  buildPathToData <- function(theYear) {
+    paste0(kPORDeepMoorPath,"DM/Environmental_Data_Deep_Moor_",theYear,".txt")
+  }
+  
+  # private function to load a year of data
+  this.loadAYearOfData <- function(theYear) {
+    if (!is.element(theYear,this.yearsOfDataAlreadyLoaded)) {  
+      this.yearsOfDataAlreadyLoaded <<- c(this.yearsOfDataAlreadyLoaded,theYear)
+      dataPathName <- buildPathToData(theYear)
+      thisyearofdata <- read.delim(dataPathName, stringsAsFactors=FALSE)
+      names(thisyearofdata)[1] <- "datetime"
+      thisyearofdata$datetime <- ymd_hms(thisyearofdata$datetime)
+      # <<- is used to assign a value to a variable located in the parent environment
+      YearsOfData <<- rbind(YearsOfData,thisyearofdata)
+    }
+    return(YearsOfData)
+  }
+  
+  # create a list of public functions for this object
+  publicFunctions <- list (
     # define the environment where this list is defined for later reference
     thisEnv = thisEnv,
     
     getEnv = function() return(get("thisEnv",thisEnv)),
     
-    yearsOfData = function() {
+    YearsOfData = function() {
       return(get("YearsOfData",thisEnv))
     },
     
-    loadAYearOfData = function(theYear) {
-      dataPathName <- paste0(kPORDeepMoorPath,"DM/Environmental_Data_Deep_Moor_",theYear,".txt")
-      thisyearofdata <- read.delim(dataPathName, stringsAsFactors=FALSE)
-      names(thisyearofdata)[1] <- "datetime"
-      thisyearofdata$datetime <- ymd_hms(thisyearofdata$datetime)
-      cumulativeyearsofdata <- rbind(YearsOfData,thisyearofdata)
-      return(assign("YearsOfData",cumulativeyearsofdata,thisEnv))
+    # this will return a range of data from the Lake POR data site
+    getPORDataRange = function (the.start.date,the.end.date) {
+      requestedYears <- as.character(c(year(the.start.date):year(the.end.date)))
+      tmp.null <- lapply(requestedYears,this.loadAYearOfData)
+      por.weather.data <- YearsOfData
+      por.weather.data <- por.weather.data[por.weather.data$datetime >= the.start.date,]
+      por.weather.data <- por.weather.data[por.weather.data$datetime <= (the.end.date + days(1)),]
+      return(por.weather.data)
     },
     
+    # This appends a year of data into YearsOfData
+    loadAYearOfData = function(theYear) {
+      this.loadAYearOfData(theYear)
+      return(assign("YearsOfData",YearsOfData,thisEnv))
+    },
+    
+    # this returns a list of available years
     listAvailableYears = function() {
       return(get("availableYearsOfData",thisEnv))
     }
@@ -59,8 +87,8 @@ LakePendOreilleData <- function() {
   )
   
   # set the name of the class
-  class(me) <- append(class(me),"LakePendOreilleData")
-  return(me)
+  class(publicFunctions) <- append(class(publicFunctions),"LakePendOreilleData")
+  return(publicFunctions)
   
   
 }
