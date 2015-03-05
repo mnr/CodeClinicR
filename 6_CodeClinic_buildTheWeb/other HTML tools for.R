341b1ff905a5f95659748be9cf3df4d3e4ce0de4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2015-03-05 12:52:59 -0800

    other tools to be used for HTML

---
+++ 6_CodeClinic_buildTheWeb/other HTML tools for.R
@@ -0,0 +1,11 @@
+#####
+# use this command to search help for concepts
+??html
+
+#####
+# htmltools - html builder tool
+# knitr - used to document R programs and their results
+# markdown - an alternative to HTML
+# RCurl - submit html to a web form
+# Shiny - robust HTML builder
+
