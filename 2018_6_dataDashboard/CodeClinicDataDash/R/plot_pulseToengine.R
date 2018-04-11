a02bfa5e6960b508b61bf25d0c109a8397f76808
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-11 15:10:40 -0700

    add symbols, color, dot size and time on x axis

--- 2018_6_dataDashboard/CodeClinicDataDash/R/plot_pulseToengine.R
+++ 2018_6_dataDashboard/CodeClinicDataDash/R/plot_pulseToengine.R
@@ -6,22 +6,35 @@
 
 #' plot pulsometer against engine efficiency
 #'
-#' @param pulsometer_10 a vector of 10 values from pulsometer.
-#' @param engine_10 a vector of 10 values from engine efficiency.
+#' @param howManyToPlot how many points to plot. Default 10
 #'
-#' @return a plot
+#' @return a plot with splines
 #'
 #' @examples
 #' plot_pulseToengine()
 
-plot_pulseToengine <- function() {
-  maybe_ten_rows <- getDashBoardDataRow(10)
-  pulse_tmp <- maybe_ten_rows$Pulsometer_readout
-  efficiency_tmp <- maybe_ten_rows$Engine_efficiency
+plot_pulseToengine <- function(howManyToPlot = 10) {
+  maybe_ten_rows <- getDashBoardDataRow(howManyToPlot)
+  timeStamp_num <- maybe_ten_rows$time_stamp
+  timeStamp_str <- strftime(maybe_ten_rows$time_stamp, format = "%H:%M:%S")
+  engineEff <- maybe_ten_rows$Engine_efficiency
+  pulseMeter <- maybe_ten_rows$Pulsometer_readout
+  redVal <- maybe_ten_rows$red_Value
+  blueVal <- maybe_ten_rows$blue_Value
+  greenVal  <- maybe_ten_rows$green_Value
 
-  plot(pulse_tmp, efficiency_tmp)
-  lines(spline(pulse_tmp, efficiency_tmp, method = 'natural', n = 100))
+  plot(timeStamp_num, engineEff,
+       ylim = c(70, 90))
 
-  title("this is a title")
+  symbols(timeStamp_num, engineEff,
+          xlab = "Time",
+          ylab = "Engine Efficiency",
+          inches = 1/5,
+          circles = pulseMeter,
+          bg = rgb(redVal/255, blueVal/255, greenVal/255),
+          xaxt = "n"
+          )
+  lines(spline(timeStamp_num, engineEff, method = 'natural', n = 100))
+  axis(1,at = timeStamp_num, labels = timeStamp_str)
 
   }
