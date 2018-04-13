e8e5c32e6cfc729cd56359e0054dc16d96d909e7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-13 11:56:38 -0700

    Notes on moving from baseR dates to lubridate

--- 2018_1_summarizeTheWeather/exploration_attempts/addLubridate.R
+++ 2018_1_summarizeTheWeather/exploration_attempts/addLubridate.R
@@ -5,10 +5,10 @@
 minDate = strptime(LPO_weather_data[ 1, "date" ], 
                    format = "%Y_%m_%d" )
 # lubridate version
-minDate = as_date(LPO_weather_data[ 1, "date" ])
+minDate = ymd(LPO_weather_data[ 1, "date" ])
 
 # original version
 maxDate = strptime(LPO_weather_data[ nrow(LPO_weather_data), "date" ],
                    format = "%Y_%m_%d" )
 # lubridate version
-maxDate = as_date(LPO_weather_data[ nrow(LPO_weather_data), "date" ])
+maxDate = ymd(LPO_weather_data[ nrow(LPO_weather_data), "date" ])
