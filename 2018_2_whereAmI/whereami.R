b8efb00fdd4bae7575bcc12d4657c4dd6cec2876
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-23 15:25:04 -0700

    freegeoip.net is deprecated. Replaced with whatismyipaddress and ipinfo.io

--- 2018_2_whereAmI/whereami.R
+++ 2018_2_whereAmI/whereami.R
@@ -25,22 +25,27 @@ library(rjson)
 library(maps)
 library(mapdata)
 
-# Geolocation -------------------------------------------------------------
+# get Geolocation -------------------------------------------------------------
 
-
-myLocation <- fromJSON(readLines("http://freegeoip.net/json/"))
+myIPaddress <- readLines("http://ipv4bot.whatismyipaddress.com/", warn = FALSE)
+getGeoString <- paste0("https://ipinfo.io/", myIPaddress,"/json")
+myLocation <- fromJSON(file = getGeoString)
+latlong <- strsplit(myLocation$loc, ",")
+myLongitude <- as.numeric(latlong[[1]][2])
+myLatitude <- as.numeric(unlist(latlong)[1])
 
 maprange = 5
 
-# dev.new(width=5, height=4) adjusts size of plotted map
+# map the location --------------------------------------------------------
+
 
 map("worldHires", 
-    xlim = c(myLocation$longitude-maprange, myLocation$longitude+maprange),
-    ylim = c(myLocation$latitude-maprange, myLocation$latitude+maprange),
+    xlim = c(myLongitude-maprange, myLongitude+maprange),
+    ylim = c(myLatitude-maprange, myLatitude+maprange),
     col = "gray80", fill = TRUE,
     mar = c(1.1, 1.1, par("mar")[3], 2))
-points(myLocation$longitude, myLocation$latitude, col="red", pch = 8, cex = 1)
-title(paste(myLocation$city, ", ", myLocation$region_code, " - ", myLocation$country_name))
+points(myLongitude, myLatitude, col="red", pch = 8, cex = 1)
+title(paste(myLocation$city, ", ", myLocation$region, " - ", myLocation$country))
 map.cities( label = TRUE, minpop = 500000)
 
 
