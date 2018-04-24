caa0bea9c4fa6617319af8b8914171d19bad265d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-24 11:05:32 -0700

    clean up comments.

--- 2018_3_eight_queens/EightQueens.R
+++ 2018_3_eight_queens/EightQueens.R
@@ -1,3 +1,7 @@
+
+# remarks -----------------------------------------------------------------
+
+
 # Copyright Mark Niemann-Ross, 2018
 # Author: Mark Niemann-Ross. mark.niemannross@gmail.com
 # LinkedIn: https://www.linkedin.com/in/markniemannross/
@@ -18,10 +22,10 @@ fail_dupRows <- c(1,1,2,3,4,5,6,7) # fails because of duplicate rows
 validSolution <- c(7,5,3,1,6,8,2,4) # This should work
 
 
-# Solution ----------------------------------------------------------------
+# Find diagonal conflicts ----------------------------------------------------------------
 
 # createDiagValues returns a vector identifying diagonal attack rows
-# ideas for speeding this up: cache diagonals
+# idea for speeding this up: cache diagonals
 createDiagValues <- function(select_row, select_col, up_or_down = 1) {
   padWithZeros <- c(rep.int(0,times = select_col))
 
@@ -36,8 +40,7 @@ createDiagValues <- function(select_row, select_col, up_or_down = 1) {
 
 
 is_this_a_valid_8_queens <- function(potentialSolution) {
-  # potentialSolution is a vector of 8 numbers representing row of each column
-  
+
   # check for diagonal conflicts
   for (eachElement in 1:8) {
     diagonal_conflicts <- createDiagValues(potentialSolution[eachElement], eachElement, 1)
@@ -74,8 +77,9 @@ runQueen <- function() {
 
 runQueen()
 
-# Plot the solutions ------------------------------------------------------
 allValidSolutions <- allPossibleSolutions[validSolutions]
+
+# Plot the solutions ------------------------------------------------------
 
 progressBar <- txtProgressBar(style = 3)
 for (queenPlot in 1:length(allValidSolutions)) {
